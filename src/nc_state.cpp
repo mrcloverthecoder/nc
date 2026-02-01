ae6d15d454dd8b3bdca95866e039b6c2bdb3d512
vixen256 <64546358+vixen256@users.noreply.github.com>
2026-02-02 10:39:35 +1300

    Add GetStateGameStyle dllexport

--- src/nc_state.cpp
+++ src/nc_state.cpp
@@ -359,6 +359,11 @@ extern "C" {
 		return &state;
 	}
 
+	__declspec(dllexport) int32_t GetStateGameStyle()
+	{
+		return GetState()->GetGameStyle();
+	}
+
 	__declspec(dllexport) bool SetStateSong(int32_t pv, int32_t difficulty, int32_t edition, int32_t style) {
 		if (const auto* entry = db::FindSongEntry(pv); entry != nullptr) {
 			GetState()->nc_song_entry = *entry;
